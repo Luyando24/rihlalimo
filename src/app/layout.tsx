6b0d9ec6f35eab33cbebbfcf799585aa91472c7e
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2026-04-08 23:18:07 +0200

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,9 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  verification: {
+    google: 'XFapRgFnLvKGxd2doGCrFM-wSIcDANkeinooDoe6YRY',
+  },
   icons: {
     icon: "/favicon.ico",
   },
